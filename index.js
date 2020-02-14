3dbcfafe731517efbed1f2dc9f8961b9f5b9894c
Milo Rastgoo <hi@informagician.com>
2020-02-14 11:05:46 -0500

    initial commit - npm installed + nodemon + express

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const express = require('express');
+const server = express();
+const port = 5000;
+server.listen(port, () => {
+    console.log(`\n* Server Running on Port ${port}*\n`)
+})
